9bdfc35c55de3dadc1f083c3aa7d0014c0cff757
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-10-19 20:27:10 +0300

    Улучшает код делегирования.

--- js/picture.js
+++ js/picture.js
@@ -25,20 +25,11 @@
     window.bigPicture.activate(window.data.currentPhoto);
   };
   var onPicturesClick = function (evt) {
-    console.log('click picturesElement', evt.target, evt.currentTarget);
-    switch (true) {
-      case (evt.target.classList.contains('picture__img')):
-        console.log('click if', evt.target);
-        findAndOpenBigPicture(evt.target.getAttribute('src'));
-        break;
-      case (evt.target.classList.contains('picture__comments') || evt.target.classList.contains('picture__likes')):
-        console.log('click if', evt.path[2]);
-        findAndOpenBigPicture(evt.path[2].querySelector('.picture__img').getAttribute('src'));
-        break;
-      case (evt.target.classList.contains('picture__info')):
-        console.log('click if', evt.path[1]);
-        findAndOpenBigPicture(evt.path[1].querySelector('.picture__img').getAttribute('src'));
-        break;
+    var picture = evt.path.find(function (it) {
+      return (it.classList) ? it.classList.contains('picture') : false;
+    });
+    if (picture) {
+      findAndOpenBigPicture(picture.querySelector('.picture__img').getAttribute('src'));
     }
   };
   picturesElement.addEventListener('click', onPicturesClick);
