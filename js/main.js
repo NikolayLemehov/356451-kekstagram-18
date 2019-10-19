a349cd40821ec4d9f1b894224e2930abc426e761
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-10-19 03:38:24 +0300

    Создаёт data.js и util.js.

--- js/main.js
+++ js/main.js
@@ -1,124 +1,8 @@
 'use strict';
 
 var ESC_KEY_CODE = 27;
-var PHOTO_QUANTITY = 25;
-var AVATAR_QUANTITY = 6;
-var SRC_PHOTO_ITEMS = ['photos/', '.jpg'];
-var SRC_AVATAR_ITEMS = ['img/avatar-', '.svg'];
-var DESCRIPTION = [
-  'Тестим новую камеру!',
-  'Угадайте, где я',
-  'Говорю “да” новым приключениям',
-  'Нечего добавить',
-  'Да, еще одно фото',
-  'Разве не потрясающе?',
-  'Что вы об этом думаете?',
-];
-var LIKES_MIN = 15;
-var LIKES_MAX = 200;
-var COMMENT_MAX = 5;
-var MESSAGE_MAX = 2;
-var MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-var NAMES = [
-  'Виктор',
-  'Олег',
-  'Илья',
-  'Анна',
-  'Татьяна',
-  'Полина'
-];
 var FIRST_INDEX = 0;
 var COEFFICIENT_MAX = 1;
-
-var findRandomInteger = function (min, max) {
-  return Math.floor(min + Math.random() * (max + 1 - min));
-};
-
-var shuffle = function (array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var swap = array[j];
-    array[j] = array[i];
-    array[i] = swap;
-  }
-  return array;
-};
-
-var createArraySerialIntegerSrc = function (quantity, srcItems) {
-  var array = [];
-  for (var i = 0; i < quantity; i++) {
-    array.push(srcItems[0] + (i + 1) + srcItems[1]);
-  }
-  return array;
-};
-
-var concatenateItemArray = function (array) {
-  var string = '' + array[0];
-  if (array.length > 1) {
-    for (var i = 1; i < array.length; i++) {
-      string = string + ' ' + array[i];
-    }
-  }
-  return string;
-};
-
-var convertPixelToInteger = function (string) {
-  return Number(string.slice(0, -2));
-};
-
-var findRandomItemArray = function (array) {
-  return array[findRandomInteger(0, array.length - 1)];
-};
-
-var findRandomSliceArray = function (array) {
-  return array.slice(findRandomInteger(0, array.length - 1));
-};
-
-var findSliceShuffleArray = function (array, sliceNumber) {
-  if (sliceNumber < array.length) {
-    return shuffle(array).slice(array.length - findRandomInteger(1, sliceNumber));
-  }
-  return shuffle(array).slice(findRandomInteger(0, array.length - 1));
-};
-
-var createDataComments = function () {
-  var dataComments = [];
-  var avatarSrcs = createArraySerialIntegerSrc(AVATAR_QUANTITY, SRC_AVATAR_ITEMS);
-  for (var i = 0; i < COMMENT_MAX; i++) {
-    var indexCommentWriter = findRandomInteger(0, AVATAR_QUANTITY - 1);
-    var dataComment = {
-      'avatar': avatarSrcs[indexCommentWriter],
-      'message': concatenateItemArray(findSliceShuffleArray(MESSAGES, MESSAGE_MAX)),
-      'name': NAMES[indexCommentWriter]
-    };
-    dataComments.push(dataComment);
-  }
-  return dataComments;
-};
-
-var createDataPhotos = function () {
-  var dataPhotos = [];
-  var photoSrcs = createArraySerialIntegerSrc(PHOTO_QUANTITY, SRC_PHOTO_ITEMS);
-  for (var i = 0; i < PHOTO_QUANTITY; i++) {
-    var dataPhoto = {
-      'url': photoSrcs[i],
-      'description': findRandomItemArray(DESCRIPTION),
-      'likes': findRandomInteger(LIKES_MIN, LIKES_MAX),
-      'comments': findRandomSliceArray(createDataComments())
-    };
-    dataPhotos.push(dataPhoto);
-  }
-  return dataPhotos;
-};
-
-var dataPhotos = createDataPhotos();
 
 var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 var picturesElement = document.querySelector('.pictures');
@@ -234,8 +118,8 @@ var imgUploadPreviewElement = document.querySelector('.img-upload__preview');
 var effectLevelLineElement = document.querySelector('.effect-level__line');
 var effectLevelPinElement = effectLevelLineElement.querySelector('.effect-level__pin');
 effectLevelPinElement.addEventListener('mouseup', function () {
-  var coefficient = convertPixelToInteger(getComputedStyle(effectLevelPinElement).left) /
-    convertPixelToInteger(getComputedStyle(effectLevelLineElement).width);
+  var coefficient = window.util.convertPixelToInteger(getComputedStyle(effectLevelPinElement).left) /
+    window.util.convertPixelToInteger(getComputedStyle(effectLevelLineElement).width);
   var checkedElement = findCheckedElement(effectsRadios);
   imgUploadPreviewElement.style.filter = mapEffect[checkedElement.getAttribute('value')](coefficient);
 });
@@ -314,15 +198,14 @@ var validateHashtags = function () {
   });
   textHashtagsInput.setCustomValidity(errorMessage);
 };
-validateHashtags();
 
 textHashtagsInput.addEventListener('change', function () {
   validateHashtags();
 });
 
-appendPhotosFragment(dataPhotos);
+appendPhotosFragment(window.data.photos);
 // bigPictureElement.classList.remove('hidden');
-fillBigPicture(bigPictureElement, dataPhotos[FIRST_INDEX]);
-appendSocialComments(dataPhotos[FIRST_INDEX].comments);
+fillBigPicture(bigPictureElement, window.data.photos[FIRST_INDEX]);
+appendSocialComments(window.data.photos[FIRST_INDEX].comments);
 bigPictureElement.querySelector('.social__comment-count').classList.add('visually-hidden');
 bigPictureElement.querySelector('.comments-loader').classList.add('visually-hidden');
---
+++ js/util.js
@@ -0,0 +1,49 @@
+'use strict';
+
+(function () {
+  window.util = {
+    findRandomInteger: function (min, max) {
+      return Math.floor(min + Math.random() * (max + 1 - min));
+    },
+    shuffle: function (array) {
+      for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var swap = array[j];
+        array[j] = array[i];
+        array[i] = swap;
+      }
+      return array;
+    },
+    createArraySerialIntegerSrc: function (quantity, srcItems) {
+      var array = [];
+      for (var i = 0; i < quantity; i++) {
+        array.push(srcItems[0] + (i + 1) + srcItems[1]);
+      }
+      return array;
+    },
+    concatenateItemArray: function (array) {
+      var string = '' + array[0];
+      if (array.length > 1) {
+        for (var i = 1; i < array.length; i++) {
+          string = string + ' ' + array[i];
+        }
+      }
+      return string;
+    },
+    convertPixelToInteger: function (string) {
+      return Number(string.slice(0, -2));
+    },
+    findRandomItemArray: function (array) {
+      return array[this.findRandomInteger(0, array.length - 1)];
+    },
+    findRandomSliceArray: function (array) {
+      return array.slice(this.findRandomInteger(0, array.length - 1));
+    },
+    findSliceShuffleArray: function (array, sliceNumber) {
+      if (sliceNumber < array.length) {
+        return this.shuffle(array).slice(array.length - this.findRandomInteger(1, sliceNumber));
+      }
+      return this.shuffle(array).slice(this.findRandomInteger(0, array.length - 1));
+    },
+  };
+})();
