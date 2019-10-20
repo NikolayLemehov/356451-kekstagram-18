2d40e0daeefd7434f52518bcd4b1a7965b43f562
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-10-21 00:42:14 +0300

    Добавляет файл choose-image.js для просмотра загружаемых картинок с помощью FileReader.

---
+++ js/chooser-image.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var uploadFileInput = document.querySelector('#upload-file');
+  var imgUploadPreviewElement = document.querySelector('.img-upload__preview img');
+  var effectsPreviewElements = document.querySelectorAll('.effects__list .effects__preview');
+
+  uploadFileInput.addEventListener('change', function () {
+    var file = uploadFileInput.files[0];
+    var fileName = file.name.toLowerCase();
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+    if (matches) {
+      var reader = new FileReader();
+      reader.addEventListener('load', function () {
+        imgUploadPreviewElement.src = reader.result;
+        getPreviewImage(reader.result);
+      });
+      reader.readAsDataURL(file);
+    }
+  });
+  var getPreviewImage = function (src) {
+    effectsPreviewElements.forEach(function (it) {
+      it.style.backgroundImage = 'url(' + src + ')';
+    });
+  };
+})();
