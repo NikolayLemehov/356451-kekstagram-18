64277240f0b7ff572518849a666cffdb93117268
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-10-20 04:58:57 +0300

    Добавляет возможность появление сообщения об ошибке.

--- js/error.js
+++ js/error.js
@@ -1,9 +1,46 @@
 'use strict';
 
 (function () {
-  window.error = {
-    onError: function () {
+  var mainElement = document.querySelector('main');
+  var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+  var addError = function () {
+    var element = errorTemplate.cloneNode(true);
+    element.style.display = 'none';
+    mainElement.appendChild(element);
+    return mainElement.querySelector('.error');
+  };
+  var errorElement = addError();
 
+  var errorTitleElement = errorElement.querySelector('.error__title');
+  var errorTryAgainBtn = errorElement.querySelector('.error__button--try-again');
+  // var errorUploadOtherBtn = errorElement.querySelector('.error__button--upload-other');
+  // errorUploadOtherBtn.setAttribute('disabled', 'disabled');
+
+  var onErrorTryAgainBtnClick = function (evt) {
+    evt.preventDefault();
+    hideErrorElement();
+  };
+  errorTryAgainBtn.addEventListener('click', onErrorTryAgainBtnClick);
+
+  var onDocumentErrorEscKeyDown = function (evt) {
+    if (evt.keyCode === window.util.ESC_KEY_CODE) {
+      hideErrorElement();
+    }
+  };
+  var showError = function () {
+    errorElement.style.display = 'block';
+    document.addEventListener('keydown', onDocumentErrorEscKeyDown);
+  };
+  var hideErrorElement = function () {
+    errorElement.style.display = 'none';
+    document.removeEventListener('keydown', onDocumentErrorEscKeyDown);
+  };
+  // showError();
+
+  window.error = {
+    onError: function (errorMessage) {
+      showError();
+      errorTitleElement.textContent = errorMessage;
     },
   };
 })();
