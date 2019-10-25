d8f1301d3345b309f593aeb18a311878d15bdc57
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-10-25 23:41:57 +0300

    Переименовывает onServer на addServerListener

--- js/backend.js
+++ js/backend.js
@@ -3,7 +3,7 @@
 (function () {
   var SUCCESS_CODE = 200;
   var MAXIMUM_EXPECTATION = 10000;
-  var onServer = function (xhr, onSuccess, onError) {
+  var addServerListener = function (xhr, onSuccess, onError) {
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
       if (xhr.status === SUCCESS_CODE) {
@@ -24,14 +24,14 @@
     load: function (onSuccess, onError) {
       var URL = 'https://js.dump.academy/kekstagram/data';
       var xhr = new XMLHttpRequest();
-      onServer(xhr, onSuccess, onError);
+      addServerListener(xhr, onSuccess, onError);
       xhr.open('GET', URL);
       xhr.send();
     },
     save: function (data, onSuccess, onError) {
       var URL = 'https://js.dump.academy/kekstagram';
       var xhr = new XMLHttpRequest();
-      onServer(xhr, onSuccess, onError);
+      addServerListener(xhr, onSuccess, onError);
       xhr.open('POST', URL);
       xhr.send(data);
     },
